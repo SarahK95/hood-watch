fca914f261def719fe9c9b65537c48dc3ef22da7
SarahKamunya <sarahkamunya254@gmail.com>
2022-06-19 17:31:57 +0300

    add  @classmethod decorator for update hood method, to update hood from db

--- watch/models.py
+++ watch/models.py
@@ -4,16 +4,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Profile(models.Model):
-    name = models.CharField(max_length=100)
-    username = models.ForeignKey(User,  on_delete=models.CASCADE)
-    bio = models.TextField(blank= True)
-    email = models.EmailField()
-    neighbourhood = models.ForeignKey(Hood, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return self.name
-    
 class Hood (models.Model):
     hood_name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
@@ -29,6 +19,34 @@ class Hood (models.Model):
     @classmethod
     def delete_hood(cls, hood_name):
         cls.objects.filter(hood_name=hood_name).delete()
+    
+    @classmethod
+    def find_hood(cls, search_term):
+        hoods = cls.cls.objects.filter(neighbourhood_name__icontains = search_term)
+        return hoods
+    
+    
+    def update_hood(self, hood_name):
+        self.hood_name = hood_name
+        self.save()
+                
+        
+        
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.ForeignKey(User,  on_delete=models.CASCADE)
+    bio = models.TextField(blank= True)
+    email = models.EmailField()
+    neighbourhood = models.ForeignKey(Hood, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+    
+# class Business(models.Model):
+        
+    
+
             
     
  
