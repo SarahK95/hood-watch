d34de5d1fed912e4ba5975a701575979443319b4
SarahKamunya <sarahkamunya254@gmail.com>
2022-06-19 17:02:01 +0300

    add class model hood with hood name,location,user fields

--- watch/models.py
+++ watch/models.py
@@ -14,19 +14,19 @@ class Profile(models.Model):
     def __str__(self):
         return self.name
     
-# class Hood (models.Model):
-#     hood_name = models.CharField(max_length=100)
-#     location = models.CharField(max_length=100)
-#     Occupants_count = models.CharField(max_length=50, blank=True)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+class Hood (models.Model):
+    hood_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    Occupants_count = models.CharField(max_length=50, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
         
-#     def __str__(self):
-#         return self.hood_name
+    # def __str__(self):
+    #     return self.hood_name
     
-#     def create_hood(self):
-#         self.save
+    # def create_hood(self):
+    #     self.save
     
-#     def 
+    # def 
             
     
  
