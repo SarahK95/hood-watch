479e1f253888d682935454f9a8d97c91e285d273
SarahKamunya <sarahkamunya254@gmail.com>
2022-06-19 17:03:01 +0300

    add create_hood method to save hoods in the db

--- watch/models.py
+++ watch/models.py
@@ -20,11 +20,11 @@ class Hood (models.Model):
     Occupants_count = models.CharField(max_length=50, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
         
-    # def __str__(self):
-    #     return self.hood_name
+    def __str__(self):
+        return self.hood_name
     
-    # def create_hood(self):
-    #     self.save
+    def create_hood(self):
+        self.save
     
     # def 
             
