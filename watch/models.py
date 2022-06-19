334e1853e0df1a77e0fe0fa2236ae187c8ee0598
SarahKamunya <sarahkamunya254@gmail.com>
2022-06-19 17:00:20 +0300

    add Profile model with name, bio,username, email fields

--- watch/models.py
+++ watch/models.py
@@ -1,9 +1,33 @@
-# from tkinter import CASCADE
-# from django.db import models
-# from django.contrib.auth.model
 
-# # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
 
-# class Profile(models.Model):
-#     name = models.CharField(max_length=100)
-#     username = models.ForeignKey(User on_delete=models.CASCADE)
+# Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.ForeignKey(User,  on_delete=models.CASCADE)
+    bio = models.TextField(blank= True)
+    email = models.EmailField()
+    neighbourhood = models.ForeignKey(Hood, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+# class Hood (models.Model):
+#     hood_name = models.CharField(max_length=100)
+#     location = models.CharField(max_length=100)
+#     Occupants_count = models.CharField(max_length=50, blank=True)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+        
+#     def __str__(self):
+#         return self.hood_name
+    
+#     def create_hood(self):
+#         self.save
+    
+#     def 
+            
+    
+ 
+    
