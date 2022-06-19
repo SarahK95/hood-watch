adf202444db72c864af74562b88cbb65ff8c6449
SarahKamunya <sarahkamunya254@gmail.com>
2022-06-20 00:02:49 +0300

    add and register models in admin.py

--- watch/admin.py
+++ watch/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
+
+
+class HealthAdmin(admin.ModelAdmin):
+    filter_horizontal =['healthservices']
 
 # Register your models here.
+admin.site.register(Hood)
+admin.site.register(Health,HealthAdmin)
+admin.site.register(Business)
+admin.site.register(Healthdepts)
+admin.site.register(Police)
+admin.site.register(Post)
+admin.site.register(Profile)
