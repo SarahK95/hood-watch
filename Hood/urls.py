a4cf44c32177bec66a4caa3b9e3f2f999ee37f7b
SarahKamunya <sarahkamunya254@gmail.com>
2022-06-19 16:16:35 +0300

    add templates folder, regstration folder wih login, registration form files

--- Hood/urls.py
+++ Hood/urls.py
@@ -14,8 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+
+from django.conf.urls import url, include
+
+
+from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url(r'', include('watch.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/',auth_views.LogoutView.as_view(),{'next_page':'/'}),
+    
+    
+
 ]
--- watch/models.py
+++ watch/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+# from tkinter import CASCADE
+# from django.db import models
+# from django.contrib.auth.model
 
-# Create your models here.
+# # Create your models here.
+
+# class Profile(models.Model):
+#     name = models.CharField(max_length=100)
+#     username = models.ForeignKey(User on_delete=models.CASCADE)
