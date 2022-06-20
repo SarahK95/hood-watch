5b4b113cc3ab0350d2dc6e8c53366dd9cd4d431c
SarahKamunya <sarahkamunya254@gmail.com>
2022-06-20 23:05:18 +0300

    add def tear down for tests

--- watch/tests.py
+++ watch/tests.py
@@ -10,8 +10,6 @@ class HoodTestClass(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.Kahawa,Hood))
 
-    def tearDown(self):
-        Hood.objects.all().delete()
 
     def test_save_method(self):
         self.Kahawa.save_neighbourhood()
@@ -22,6 +20,9 @@ class HoodTestClass(TestCase):
         self.Kahawa.delete_neighbourhood('Buru')
         hood = Hood.objects.all()
         self.assertTrue(len(hood)==0)
+        
+    def tearDown(self):
+            Hood.objects.all().delete()    
 
 class HealthDeptsTestClass(TestCase):
     def setUp(self):
@@ -30,8 +31,6 @@ class HealthDeptsTestClass(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.Dentist,Healthdepts))
 
-    def tearDown(self):
-        Healthdepts.objects.all().delete()
 
     def test_save_method(self):
         self.Dentist.save_healthdepts()
@@ -43,6 +42,9 @@ class HealthDeptsTestClass(TestCase):
         health = Healthdepts.objects.all()
         self.assertTrue(len(health)==0)
         
+    def tearDown(self):
+        Healthdepts.objects.all().delete()    
+        
 class BusinessTestClass(TestCase):
     def setUp(self):
         self.Dentist = Business(Health='Fashion')
@@ -50,15 +52,15 @@ class BusinessTestClass(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.Dentist,Business))
 
-    def tearDown(self):
-        Business.objects.all().delete()
-
     def test_save_method(self):
         self.Fashion.save_business()
         biz = Business.objects.all()
         self.assertTrue(len(biz)>0)
 
     def test_delete_method(self):
-        self.Fashion.delete_healthdepts('Fashion')
+        self.Fashion.delete_business('Fashion')
         biz = Business.objects.all()
-        self.assertTrue(len(biz)==0)        
+        self.assertTrue(len(biz)==0) 
+        
+    def tearDown(self):
+        Business.objects.all().delete()           
