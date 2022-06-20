aef72ea4151ca6e1cbe6b397b3119a015550ef1c
SarahKamunya <sarahkamunya254@gmail.com>
2022-06-20 13:02:25 +0300

    add columns in index html for the models

--- watch/urls.py
+++ watch/urls.py
@@ -5,9 +5,18 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.home, name= 'home'),
-    url(r'^create/profile$',views.create_profile, name='create-profile'),
+    url(r'^create/profile$',views.create_profile, name='create-prof'),
     url(r'^post',views.post, name='post'),
     url(r'^businesses',views.businesses, name='businesses'),
+    url(r'^health',views.health, name='health'),
+    url(r'^police',views.police, name='police'),
+    url(r'^search/',views.search_results, name='search_results'),
+    url(r'^update/profile$',views.update_profile, name='update-profile'),
+    url(r'^new/business$',views.new_biz, name='new-biz'),
+    url(r'^new/post$',views.new_post, name='new-post'),
+    url(r'^user/(?P<username>\w{0,50})',views.user_profile,name='user-profile'),
+    url(r'^my-profile/',views.my_prof, name='my-prof'),
+    url(r'^view/blog/(\d+)',views.view_post,name='view_post'),
    
  
 ]
